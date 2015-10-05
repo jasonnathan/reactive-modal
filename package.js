4caea3daea0c05ac4b184daa61e4fdf11187c7b1
Josh <c13joshua.christman@gmail.com>
2015-10-05 17:58:41 -0500

    Update depedencies and version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Reactive bootstrap modals for meteor",
-  version: "1.0.4",
+  version: "1.0.5",
   git: "https://github.com/jchristman/reactive-modal.git",
   name: "jchristman:reactive-modal"
 });
@@ -9,7 +9,7 @@ Package.on_use(function (api) {
   if(api.versionsFrom){
     api.versionsFrom('METEOR@1.0');
   }
-  api.use(['underscore', 'jquery','templating', 'reactive-var@1.0.5'], 'client');
+  api.use(['underscore', 'jquery','templating', 'reactive-var@1.0.6'], 'client');
   api.add_files(['lib/reactive-modal.html', 'lib/reactive-modal.js', 'lib/ev.js'], "client");
   api.export('ReactiveModal', ['client']);
 });
